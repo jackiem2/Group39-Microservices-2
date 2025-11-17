c798df970847d971810c4ce417288c8954e9db82
jackiem2 <jacqueline.merlan@gmail.com>
2025-11-16 23:14:18 -0600

    Using sample data from assignment 6

--- microservice_rating_test.js
+++ microservice_rating_test.js
@@ -2,3 +2,10 @@
 const basePostUrl = "http://localhost:5002/ratings";
 const baseSummaryUrl = "http://localhost:5002/ratings/summary";
 const baseListUrl = "http://localhost:5002/ratings/list";
+
+// Using sample data from assignment 6
+const ratingData = {
+  itemId: "ITEM_123",
+  rating: 5,
+  comment: "Amazing quality!"
+};
