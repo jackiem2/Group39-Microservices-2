4bb2c305a219d0686a7ece568f9bca2c3d41da26
jackiem2 <jacqueline.merlan@gmail.com>
2025-11-16 23:08:08 -0600

    ADDED BASE URLS FOR TEST PROGRAM

---
+++ microservice_rating_test.js
@@ -0,0 +1,4 @@
+// Base URLs 
+const basePostUrl = "http://localhost:5002/ratings";
+const baseSummaryUrl = "http://localhost:5002/ratings/summary";
+const baseListUrl = "http://localhost:5002/ratings/list";
